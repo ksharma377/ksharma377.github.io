35e6146793ecd1892c081a79bc0edbb9263d8014
Kshitij Sharma <ksharma377@gmail.com>
2018-03-04 10:22:28 +0530

    Added duplicate participant check and display sorted list

--- smart-split/js/smart-split.js
+++ smart-split/js/smart-split.js
@@ -4,16 +4,23 @@ app.controller("smart-split-controller", function($scope) {
 	
 	$scope.participants = [];
 	$scope.participantName = "";
+
 	$scope.addParticipant = function() {
 		var participant = $scope.participantName;
 		if (participant.length == 0) {
 			return;
 		}
+		if ($scope.participants.indexOf(participant) > -1) {
+			alert('Participant already exists. Please use a different name.');
+			return;
+		}
 		$scope.participants.push(participant);
+		$scope.participants.sort();
 		$scope.participantName = "";
 	}
+	
 	$scope.deleteParticipant = function() {
-		
+
 	}
 
 	$scope.items = [];
